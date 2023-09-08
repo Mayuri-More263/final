2a4b000b198b15c334e8794c496c09b57c656878
MAYURI MORE <122468060+Mayuri-More263@users.noreply.github.com>
2023-09-09 00:54:19 +0530

    Update final.js

--- final.js
+++ final.js
@@ -5,3 +5,16 @@ const h2Element = document.querySelector("h2");
 if (h2Element) {
   h2Element.style.color = "green";
 }
+
+var li = document.querySelectorAll("li");
+li[2].style.backgroundColor = "green";
+titles[1].style.fontweight = "bold";
+titles[2].style.fontweight = "bold";
+items[0].style.fontweight = "bold";
+items[1].style.fontweight = "bold";
+items[2].style.fontweight = "bold";
+items[3].style.fontweight = "bold";
+
+var li = document.querySelectorAll("li");
+li[1].style.backgroundColor = "green";
+li[2].style.textcontent = "invisible";
