8968f4f11ae0c5ef0cfbcd2df2fc46d2aca8cbd9
MAYURI MORE <122468060+Mayuri-More263@users.noreply.github.com>
2023-09-08 23:45:17 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,7 @@
+// Get the <h2> element by its tag name (assuming you have an <h2> element)
+const h2Element = document.querySelector("h2");
+
+// Change the color to green
+if (h2Element) {
+  h2Element.style.color = "green";
+}
